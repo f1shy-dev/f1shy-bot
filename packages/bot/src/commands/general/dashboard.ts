60f4903a7e03321354148843d4524f422ac2da52
vishy-dev <agarwal.vrishank@gmail.com>
2021-07-16 22:00:32 +0100

    feat: invite and dashboard commands

--- packages/bot/src/commands/general/dashboard.ts
+++ packages/bot/src/commands/general/dashboard.ts
@@ -3,19 +3,19 @@ import { Command } from "@sapphire/framework";
 
 import { ApplyCustomOptions } from "../../lib/ApplyCustomOptions";
 import { BasicEmbed } from "../../lib/EmbedBuilders";
+import { CustomClient } from "../../structures/CustomClient";
 
 @ApplyCustomOptions({
-  name: "ping",
-  description: "Sends back the ping of the bot!",
+  name: "dashboard",
+  description: "Get a link to F1shyBot's dashboard!",
   category: "General",
 })
 export default class PingCommand extends Command {
   async run(message: Message): Promise<Message> {
     return await message.channel.send({
       embeds: [
-        BasicEmbed(":ping_pong: Pong!").setDescription(
-          `Bot Latency: \`${this.context.client.ws.ping}ms\`
-          API Latency: \`${Date.now() - message.createdTimestamp}ms\``
+        BasicEmbed(
+          "[Click here to go to F1shyBot's web dashboard!](https://bot.f1sh.me)"
         ),
       ],
     });
--- packages/bot/src/commands/general/invite.ts
+++ packages/bot/src/commands/general/invite.ts
@@ -5,17 +5,16 @@ import { ApplyCustomOptions } from "../../lib/ApplyCustomOptions";
 import { BasicEmbed } from "../../lib/EmbedBuilders";
 
 @ApplyCustomOptions({
-  name: "ping",
-  description: "Sends back the ping of the bot!",
+  name: "invite",
+  description: "Get a link to invite F1shyBot's to your own servers!",
   category: "General",
 })
 export default class PingCommand extends Command {
   async run(message: Message): Promise<Message> {
     return await message.channel.send({
       embeds: [
-        BasicEmbed(":ping_pong: Pong!").setDescription(
-          `Bot Latency: \`${this.context.client.ws.ping}ms\`
-          API Latency: \`${Date.now() - message.createdTimestamp}ms\``
+        BasicEmbed(
+          `[Click here to go to invite **${this.context.client.user?.username}** to your own server!](https://discord.com/api/oauth2/authorize?client_id=${this.context.client.options.api?.auth?.id}&permissions=268520566&scope=bot)`
         ),
       ],
     });
